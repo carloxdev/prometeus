2555123ecfb952f63383e1b6a177193d3c90aed2
Carlos <carloxdev@gmail.com>
2017-06-25 20:29:00 -0500

    Avance de cambios

--- editorial/views.py
+++ editorial/views.py
@@ -16,28 +16,28 @@ class NewList(View):
 
 
 class NewAdd(View):
-    template_name = "new_list.html"
+    template_name = "new_add.html"
 
     def get(self, _request):
         return render(_request, self.template_name, {})
 
 
 class NewAddSuccess(View):
-    template_name = "new_list.html"
+    template_name = "new_add_success.html"
 
     def get(self, _request):
         return render(_request, self.template_name, {})
 
 
 class NewView(View):
-    template_name = "new_list.html"
+    template_name = "new_view.html"
 
     def get(self, _request, _pk):
         return render(_request, self.template_name, {})
 
 
 class NewEdit(View):
-    template_name = "new_list.html"
+    template_name = "new_edit.html"
 
     def get(self, _request, _pk):
         return render(_request, self.template_name, {})
