910f56321a140c741aaa4f2b300d95528f17ea8d
CarloxDev <carloxdev@gmail.com>
2017-06-28 01:19:37 -0500

    Se agregaron avances

---
+++ payroll/static/voucher_add.js
@@ -0,0 +1,44 @@
+/*-----------------------------------------------*\
+            GLOBAL VARIABLES
+\*-----------------------------------------------*/
+
+// OBJS
+
+
+/*-----------------------------------------------*\
+            LOAD
+\*-----------------------------------------------*/
+
+$(document).ready(function () {
+  
+	$empleados = $('#empleados')
+	$empleados.select2({
+		theme: "bootstrap"
+	})
+
+	$fecha = $('#fecha')
+	$fecha.daterangepicker()
+
+})
+
+
+
+/*-----------------------------------------------*\
+            OBJETO: PostThree
+\*-----------------------------------------------*/
+
+// function PostThree() { 
+
+//     this.id = $('#myTree')
+
+//     this.init_Components()
+// }
+// PostThree.prototype.init_Components = function () {
+
+//     // dataSource = function(parentData, callback){
+//     //   //...
+//     // };
+
+//     this.id.tree()
+        
+// }
