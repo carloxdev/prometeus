c17ff0915d4ee4381f9e69a39afca85b7ae908b7
CarloxDev <carloxdev@gmail.com>
2017-09-21 01:49:23 -0500

    Fix a modelo profile

--- security/admin.py
+++ security/admin.py
@@ -1,6 +1,73 @@
 # -*- coding: utf-8 -*-
+
+# Python's Libraries
 from __future__ import unicode_literals
 
+# Django's Libraries
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+# Own's Libraries
+from .models import Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_superuser',
+
+        'recruited_date',
+        'birth_date',
+        'gender',
+        'job_title',
+        'department',
+        'phone',
+    )
+    list_select_related = ('profile', )
+
+    def recruited_date(self, instance):
+        return instance.profile.recruited_date
+
+    def birth_date(self, instance):
+        return instance.profile.birth_date
+
+    def gender(self, instance):
+        return instance.profile.gender
+
+    def job_title(self, instance):
+        return instance.profile.job_title
+
+    def department(self, instance):
+        return instance.profile.department
+
+    def phone(self, instance):
+        return instance.profile.phone
+
+    # get_recruited_date.short_recruited_date = "Contratacion"
+    # get_birth_date.short_birth_date = "Cumpleaños"
+    # get_gender.short_gender = "Genero"
+    # get_job_title.short_job_title = "Puesto"
+    # get_department.short_department = "Departamento"
+    # get_phone.short_phone = "Telefono"
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- security/models.py
+++ security/models.py
@@ -1,6 +1,34 @@
 # -*- coding: utf-8 -*-
+
+# Python's Libraries
 from __future__ import unicode_literals
 
+# Django's Libraries
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+
+    GENEROS = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    recruited_date = models.DateField(null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(choices=GENEROS, max_length=144, null=True, blank=True)
+    job_title = models.CharField(max_length=144, null=True, blank=True)
+    department = models.CharField(max_length=144, null=True, blank=True)
+    phone = models.CharField(max_length=144, null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
