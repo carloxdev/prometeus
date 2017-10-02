d50a51c854ff47521532e38a3b589b9cd5267351
CarloxDev <carloxdev@gmail.com>
2017-10-02 02:32:18 -0500

    Avance en exportacion de datos

--- security/admin.py
+++ security/admin.py
@@ -29,6 +29,7 @@ class UserResource(resources.ModelResource):
             'email',
             'is_active',
         )
+        skip_unchanged = True
         import_id_fields = ['username']
         export_order = (
             'username',
@@ -39,9 +40,66 @@ class UserResource(resources.ModelResource):
         )
 
 
-class UserAdmin(ImportExportModelAdmin, UserAdmin):
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(ImportExportModelAdmin, UserAdmin):
+    inlines = (ProfileInline, )
     resource_class = UserResource
-    pass
+    list_display = (
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+        'is_active',
+
+        'recruited_date',
+        'birth_date',
+        'gender',
+        'job_title',
+        'department',
+        'phone',
+        'address',
+    )
+
+    list_select_related = ('profile', )
+
+    def recruited_date(self, instance):
+        return instance.profile.recruited_date
+
+    def birth_date(self, instance):
+        return instance.profile.birth_date
+
+    def gender(self, instance):
+        return instance.profile.gender
+
+    def job_title(self, instance):
+        return instance.profile.job_title
+
+    def department(self, instance):
+        return instance.profile.department
+
+    def phone(self, instance):
+        return instance.profile.phone
+
+    def address(self, instance):
+        return instance.profile.address
+
+    # get_recruited_date.short_recruited_date = "Contratacion"
+    # get_birth_date.short_birth_date = "Cumpleaños"
+    # get_gender.short_gender = "Genero"
+    # get_job_title.short_job_title = "Puesto"
+    # get_department.short_department = "Departamento"
+    # get_phone.short_phone = "Telefono"
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
 
 
 class ProfileResource(resources.ModelResource):
@@ -95,4 +153,4 @@ class AdminProfile(ImportExportModelAdmin):
 
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
