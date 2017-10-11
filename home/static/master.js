580cd1a39fc232eb387a66631b906f3976ce18fd
Carlos <carloxdev@gmail.com>
2017-10-11 16:54:29 -0500

    Fix a problema de pace

--- home/static/master.js
+++ home/static/master.js
@@ -4,12 +4,9 @@
 $(document).ready(function () {
     window.paceOptions = {
         // Disable the 'elements' source
-        elements: true,
+        elements: false,
         // Only show the progress on regular and ajax-y page navigation,
         // not every request
-        restartOnRequestAfter: true,
-        ajax: {
-          trackMethods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH']
-        }
+        restartOnRequestAfter: false,
     }
 })
