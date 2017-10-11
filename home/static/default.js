492a2daeefec45b90fcc0c5e2984328fb070a906
Carlos <carloxdev@gmail.com>
2017-10-11 10:58:01 -0500

    Fix a estilos de carrusel

--- home/static/default.js
+++ home/static/default.js
@@ -3,9 +3,39 @@ $(document).ready(function() {
     var $carrusel = $('#carrusel')
     $carrusel.slick({
         autoplay: true,
-        autoplay: 3000,
+        autoplaySpeed: 3000,
         infinite: true,
         dots: true,
 
+        centerMode: true,
+        centerPadding: '0px',
+
+        responsive: [
+            // {
+            //     breakpoint: 1024,
+            //     settings: {
+            //         slidesToShow: 3,
+            //         slidesToScroll: 3,
+            //     }
+            // },
+            {
+                breakpoint: 600,
+                settings: {
+                    dots: false,
+                    arrows: false,
+                }
+            },
+            // {
+            //     breakpoint: 480,
+            //     settings: {
+            //         dots: false,
+            //         arrows: false,
+            //
+            //     }
+            // }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     })
 })
