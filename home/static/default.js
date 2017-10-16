497d177ea04ace60510f83c6ef1e5a62c2ec23e9
CarloxDev <carloxdev@gmail.com>
2017-10-16 02:56:51 -0500

    Se afinaron estilos y estructura de sitio

--- home/static/default.js
+++ home/static/default.js
@@ -7,35 +7,14 @@ $(document).ready(function() {
         infinite: true,
         dots: true,
 
-        // centerMode: true,
-        // centerPadding: '-10px',
-
         responsive: [
-            // {
-            //     breakpoint: 1024,
-            //     settings: {
-            //         slidesToShow: 3,
-            //         slidesToScroll: 3,
-            //     }
-            // },
             {
-                breakpoint: 600,
+                breakpoint: 768,
                 settings: {
                     dots: false,
                     arrows: false,
                 }
             },
-            // {
-            //     breakpoint: 480,
-            //     settings: {
-            //         dots: false,
-            //         arrows: false,
-            //
-            //     }
-            // }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
         ]
     })
 })
