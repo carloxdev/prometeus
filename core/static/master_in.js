26372b77c4c1604ffef4d455e034bdc40ae8e8c5
carloxdev <carloxdev@gmail.com>
2018-01-09 11:14:48 -0600

    Ultimo, ultimos cambios

--- core/static/master_in.js
+++ core/static/master_in.js
@@ -1,12 +1,1 @@
 
-/* ------------------------ LOAD ------------------------ */
-
-$(document).ready(function () {
-    window.paceOptions = {
-        // Disable the 'elements' source
-        elements: false,
-        // Only show the progress on regular and ajax-y page navigation,
-        // not every request
-        restartOnRequestAfter: false,
-    }
-})
